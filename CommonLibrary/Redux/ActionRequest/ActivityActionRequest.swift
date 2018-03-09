f528dd9dfe9b178aaf3bfae0c95c5e788912cc36
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-03-09 18:34:57 +0100

    Refactor ActivityActionRequest

--- CommonLibrary/Redux/ActionRequest/ActivityActionRequest.swift
+++ CommonLibrary/Redux/ActionRequest/ActivityActionRequest.swift
@@ -19,37 +19,6 @@ public enum ActivityActionRequest: Int, Codable {
     case healthTrackerDidFinish = 4
     case healthTrackerDidStart = 5
 }
-//
-//extension ActivityActionRequest: Codable {
-//    public init(from decoder: Decoder) throws {
-//        let container = try decoder.singleValueContainer()
-//        let value = try container.decode(Int.self)
-//        switch value {
-//        case 0: self = .startActivity
-//        case 1: self = .pauseActivity
-//        case 2: self = .finishActivity
-//        case 3: self = .resetActivity
-//        default:
-//            throw DecodingError.valueNotFound(Int.self,
-//                                              DecodingError.Context(codingPath: decoder.codingPath,
-//                                                                    debugDescription: "Only the types 0 to 3 are allowed to be codable"))
-//        }
-//    }
-//
-//    public func encode(to encoder: Encoder) throws {
-//        var container = encoder.singleValueContainer()
-//        switch self {
-//        case .startActivity: try container.encode(0)
-//        case .pauseActivity: try container.encode(1)
-//        case .finishActivity: try container.encode(2)
-//        case .resetActivity: try container.encode(3)
-//        default:
-//            throw EncodingError.invalidValue(4,
-//                                             EncodingError.Context(codingPath: encoder.codingPath,
-//                                                                   debugDescription: "Only the types 0 to 3 are allowed to be codable"))
-//       }
-//    }
-//}
 
 extension ActivityActionRequest: ActionRequest {
     public func execute(getState: @escaping () -> AppState,
@@ -57,103 +26,134 @@ extension ActivityActionRequest: ActionRequest {
                         dispatchAsync: @escaping (AnyActionAsync<AppState>) -> Void) {
         let state = getState()
 
-        switch state.deviceState.deviceType {
-        case .appleWatch:
-            handleAppleWatch(getState: getState, dispatch: dispatch, dispatchAsync: dispatchAsync)
-        case .iPhone:
-            handleiPhone(getState: getState, dispatch: dispatch, dispatchAsync: dispatchAsync)
-        }
-    }
+        switch (self, state.deviceState.deviceType) {
+        case (.startActivity, .appleWatch):
+            watchStartActivity(state: state, dispatch: dispatch)
+        case (.startActivity, .iPhone):
+            phoneStartActivity(state: state, dispatch: dispatch)
 
-    public func handleAppleWatch(getState: @escaping () -> AppState,
-                                 dispatch: @escaping DispatchFunction,
-                                 dispatchAsync: @escaping (AnyActionAsync<AppState>) -> Void) {
-        let state = getState()
+        case (.pauseActivity, .appleWatch):
+            watchPauseActivity(state: state, dispatch: dispatch)
+        case (.pauseActivity, .iPhone):
+            phonePauseActivity(state: state, dispatch: dispatch)
 
-        switch self {
-        case .startActivity:
-            guard state.deviceState.locationPermission == .authorized else { return }
-            guard state.currentActivity == nil || state.currentActivity?.state != .inProgress else { return }
+        case (.finishActivity, .appleWatch):
+            watchFinishActivity(state: state, dispatch: dispatch)
+        case (.finishActivity, .iPhone):
+            phoneFinishActivity(state: state, dispatch: dispatch)
 
-            locationTracker.start()
-            dispatch(ActivityAction.activityDidStart)
-            dispatch(RouterAction.didNavigate(.activityPhotoStream))
-            if state.currentActivity?.state == .paused {
-                healthKitTracker.resume()
-            } else {
-                healthKitTracker.start()
-            }
-        case .pauseActivity:
-            guard state.currentActivity?.state == .inProgress else { return }
+        case (.resetActivity, .appleWatch):
+            watchResetActivity(state: state, dispatch: dispatch)
+        case (.resetActivity, .iPhone):
+            phoneResetActivity(state: state, dispatch: dispatch)
 
-            locationTracker.stop()
-            dispatch(ActivityAction.activityDidPause)
-            healthKitTracker.pause()
-        case .finishActivity:
-            guard state.currentActivity?.state == .inProgress || state.currentActivity?.state == .paused else { return }
-
-            locationTracker.stop()
-            dispatch(ActivityAction.activityDidFinish)
-            healthKitTracker.stop()
-        case .resetActivity:
-            guard state.currentActivity?.state == .finished else { return }
-
-            locationTracker.stop()
-            dispatch(ActivityAction.activityDidReset)
-            healthKitTracker.reset()
-        case .healthTrackerDidStart:
-            guard let activity = state.currentActivity,
-                let startDate = activity.startDate else { return }
-            healthKitTracker.startAccumulatingData(from: startDate)
-        case .healthTrackerDidFinish:
-            guard let activity = state.currentActivity,
-                let startDate = activity.startDate,
-                let endDate = activity.endDate else { return }
-            healthKitTracker.save(from: startDate,
-                                  to: endDate,
-                                  distance: activity.totalDistance,
-                                  energy: activity.totalEnergyBurned,
-                                  locations: activity.locations)
-        }
-    }
-
-    public func handleiPhone(getState: @escaping () -> AppState,
-                             dispatch: @escaping DispatchFunction,
-                             dispatchAsync: @escaping (AnyActionAsync<AppState>) -> Void) {
-        let state = getState()
-
-        switch self {
-        case .startActivity:
-            guard state.deviceState.locationPermission == .authorized else { return }
-            remoteDevice.send(type: ActivityActionRequest.className,
-                              data: [ActivityActionRequest.className: ActivityActionRequest.startActivity]) { response in
-                if case .failure = response {
-                    print("Error controlling Apple Watch")
-                }
-            }
-        case .pauseActivity:
-            remoteDevice.send(type: ActivityActionRequest.className,
-                              data: [ActivityActionRequest.className: ActivityActionRequest.pauseActivity]) { response in
-                if case .failure = response {
-                    print("Error controlling Apple Watch")
-                }
-            }
-        case .finishActivity:
-            remoteDevice.send(type: ActivityActionRequest.className,
-                              data: [ActivityActionRequest.className: ActivityActionRequest.finishActivity]) { response in
-                if case .failure = response {
-                    print("Error controlling Apple Watch")
-                }
-            }
-        case .resetActivity:
-            remoteDevice.send(type: ActivityActionRequest.className,
-                              data: [ActivityActionRequest.className: ActivityActionRequest.resetActivity]) { response in
-                if case .failure = response {
-                    print("Error controlling Apple Watch")
-                }
-            }
-        case .healthTrackerDidStart, .healthTrackerDidFinish:
+        case (.healthTrackerDidStart, .appleWatch):
+            watchHealthTrackerDidStart(state: state, dispatch: dispatch)
+        case (.healthTrackerDidStart, .iPhone):
             break
+
+        case (.healthTrackerDidFinish, .appleWatch):
+            watchHealthTrackerDidFinish(state: state, dispatch: dispatch)
+        case (.healthTrackerDidFinish, .iPhone):
+            break
+        }
+    }
+}
+
+// MARK: - Apple Watch actions
+extension ActivityActionRequest {
+    private func watchStartActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard state.deviceState.locationPermission == .authorized else { return }
+        guard state.currentActivity == nil || state.currentActivity?.state != .inProgress else { return }
+
+        locationTracker.start()
+        dispatch(ActivityAction.activityDidStart)
+        dispatch(RouterAction.didNavigate(.activityPhotoStream))
+        if state.currentActivity?.state == .paused {
+            healthKitTracker.resume()
+        } else {
+            healthKitTracker.start()
+        }
+    }
+
+    private func watchPauseActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard state.currentActivity?.state == .inProgress else { return }
+
+        locationTracker.stop()
+        dispatch(ActivityAction.activityDidPause)
+        healthKitTracker.pause()
+    }
+
+    private func watchFinishActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard state.currentActivity?.state == .inProgress || state.currentActivity?.state == .paused else { return }
+
+        locationTracker.stop()
+        dispatch(ActivityAction.activityDidFinish)
+        healthKitTracker.stop()
+    }
+
+    private func watchResetActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard state.currentActivity?.state == .finished else { return }
+
+        locationTracker.stop()
+        dispatch(ActivityAction.activityDidReset)
+        healthKitTracker.reset()
+    }
+
+    private func watchHealthTrackerDidStart(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard let activity = state.currentActivity,
+            let startDate = activity.startDate else { return }
+        healthKitTracker.startAccumulatingData(from: startDate)
+    }
+
+    private func watchHealthTrackerDidFinish(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard let activity = state.currentActivity,
+            let startDate = activity.startDate,
+            let endDate = activity.endDate else { return }
+        healthKitTracker.save(from: startDate,
+                              to: endDate,
+                              distance: activity.totalDistance,
+                              energy: activity.totalEnergyBurned,
+                              locations: activity.locations)
+    }
+}
+
+// MARK: - iPhone actions
+extension ActivityActionRequest {
+    private func phoneStartActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        guard state.deviceState.locationPermission == .authorized else { return }
+        remoteDevice.send(type: ActivityActionRequest.className,
+                          data: [ActivityActionRequest.className: ActivityActionRequest.startActivity]) { response in
+                            if case .failure = response {
+                                print("Error controlling Apple Watch")
+                            }
+        }
+    }
+
+    private func phonePauseActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        remoteDevice.send(type: ActivityActionRequest.className,
+                          data: [ActivityActionRequest.className: ActivityActionRequest.pauseActivity]) { response in
+                            if case .failure = response {
+                                print("Error controlling Apple Watch")
+                            }
+        }
+    }
+
+    private func phoneFinishActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        remoteDevice.send(type: ActivityActionRequest.className,
+                          data: [ActivityActionRequest.className: ActivityActionRequest.finishActivity]) { response in
+                            if case .failure = response {
+                                print("Error controlling Apple Watch")
+                            }
+        }
+    }
+
+    private func phoneResetActivity(state: AppState, dispatch: @escaping DispatchFunction) {
+        remoteDevice.send(type: ActivityActionRequest.className,
+                          data: [ActivityActionRequest.className: ActivityActionRequest.resetActivity]) { response in
+                            if case .failure = response {
+                                print("Error controlling Apple Watch")
+                            }
         }
     }
 }
