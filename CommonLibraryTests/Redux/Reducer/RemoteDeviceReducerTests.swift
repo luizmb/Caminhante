189071fac413de885b8c1c4ffc0fc5b7248b3c1d
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2018-03-11 19:44:57 +0100

    RemoteDeviceReducerTests

---
+++ CommonLibraryTests/Redux/Reducer/RemoteDeviceReducerTests.swift
@@ -0,0 +1,45 @@
+//
+//  ActivityReducerTests.swift
+//  CommonLibraryTests
+//
+//  Created by Luiz Rodrigo Martins Barbosa on 11.03.18.
+//
+
+@testable import CommonLibrary
+import Foundation
+import KleinKit
+import Nimble
+
+class RemoteDeviceReducerTests: BaseTests {
+    func testActivationCompleteWithSuccess() {
+        let state = givenNoActivity()
+
+        let sut = EntryPointReducer().reduce(state, action: RemoteDeviceAction.activationComplete(success: true))
+
+        expect(sut.deviceState.counterPartConnection).to(beTrue())
+    }
+
+    func testActivationCompleteWithFailure() {
+        let state = givenNoActivity()
+
+        let sut = EntryPointReducer().reduce(state, action: RemoteDeviceAction.activationComplete(success: false))
+
+        expect(sut.deviceState.counterPartConnection).to(beFalse())
+    }
+
+    func testReachabilityCompleteWithSuccess() {
+        let state = givenNoActivity()
+
+        let sut = EntryPointReducer().reduce(state, action: RemoteDeviceAction.reachabilityChanged(isReachable: true))
+
+        expect(sut.deviceState.counterPartReachable).to(beTrue())
+    }
+
+    func testReachabilityCompleteWithFailure() {
+        let state = givenNoActivity()
+
+        let sut = EntryPointReducer().reduce(state, action: RemoteDeviceAction.reachabilityChanged(isReachable: false))
+
+        expect(sut.deviceState.counterPartReachable).to(beFalse())
+    }
+}
